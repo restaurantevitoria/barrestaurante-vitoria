32790569e991fdb99f7ef68145ed7e9fc7725731
restaurantevitoria <vbarrestaurante@gmail.com>
2024-12-11 20:27:27 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Página lista para Restaurante Vitoria");
+
+// Ejemplo de una funcionalidad: Alerta al hacer clic en un botón
+document.querySelector(".btn").addEventListener("click", function () {
+    alert("¡Descubre nuestro menú!");
+});
